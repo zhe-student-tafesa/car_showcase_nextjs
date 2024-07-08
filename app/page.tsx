ea0c85d6b7cd25dd142e72d82f966985b61e7374
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-07-08 13:49:35 +1000

    1. add SearchBar and CustomFilter in Home component;

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero } from '@/components'
+import { CustomFilter, Hero, SearchBar } from '@/components'
 import Image from 'next/image'
 
 
@@ -13,6 +13,17 @@ export default function Home() {
           <h1 className='text-4xl font-extrabold'>
             Car Catalogue
           </h1>
+          <p>
+            Explore the cars you might like
+          </p>
+        </div>
+        <div className='home__filters'>
+            <SearchBar />
+
+            <div className='home__filter-container'>
+              <CustomFilter />
+              <CustomFilter />
+            </div>
         </div>
       </div>
     </main>
