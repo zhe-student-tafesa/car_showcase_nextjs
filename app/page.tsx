4f8913e8439103231aa0fe70940846a3bb39e18b
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-07-08 13:39:21 +1000

    1. begin Catalogue;

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,14 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      {/* padding-x: defined in globals.css. when sm, px-16; other: px-6 */}
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>
+            Car Catalogue
+          </h1>
+        </div>
+      </div>
     </main>
   )
 }
