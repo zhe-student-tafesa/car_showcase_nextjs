2d767ffe2162dd324cb293665d49121a46c2e378
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-07-08 11:58:49 +1000

    1. add image resources; 2. finish image section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import CustomButton from './CustomButton'
+import Image from 'next/image'
 
 const handleScroll = () => { }
 const Hero = () => {
@@ -19,6 +20,17 @@ const Hero = () => {
                     containerStyles="bg-primary-blue text-white rounded-full mt-10"
                     handleClick={handleScroll}
                 />
+            </div>
+            <div className='hero__image-container'>
+                
+                <div className='hero__image'>
+                    <Image src="/hero.png" alt="hero"
+                    fill className='object-contain'
+                    />
+                </div>
+                <div className='hero__image-overlay'>
+
+                </div>
             </div>
         </div>
     )
