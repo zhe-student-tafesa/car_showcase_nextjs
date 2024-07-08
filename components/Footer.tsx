7312ffe8b89245efe338a8a8345f836d0b7f1faf
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-07-08 13:27:11 +1000

    1. add comments;

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,6 +8,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className='flex flex-col text-blue-100 mt-5 border-t border-gray-100'>
+            {/* col-children:1 */}
             <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
                 <div className='flex flex-col justify-start items-start gap-6'>
                     <Image
@@ -42,8 +43,9 @@ const Footer = () => {
                     ))}
                 </div>
             </div>
+            {/* col-children:2 */}
             <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
-                <p  className="text-gray-600">@2024 CarHub. All rights reserved</p>
+                <p className="text-gray-600">@2024 CarHub. All rights reserved</p>
 
                 <div className="footer__copyrights-link">
                     <Link href="/" className="text-gray-500">
