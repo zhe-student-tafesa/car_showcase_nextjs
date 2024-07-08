b1561e0d8fdeb0de1758135b474acf0a56b24d54
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-07-08 12:00:21 +1000

    finish image section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,14 +22,13 @@ const Hero = () => {
                 />
             </div>
             <div className='hero__image-container'>
-                
+
                 <div className='hero__image'>
                     <Image src="/hero.png" alt="hero"
-                    fill className='object-contain'
+                        fill className='object-contain'
                     />
                 </div>
                 <div className='hero__image-overlay'>
-
                 </div>
             </div>
         </div>
