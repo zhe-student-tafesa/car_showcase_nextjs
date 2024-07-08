01c70400c96d96d01dabd6c3c6d3263159b5b669
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-07-08 11:13:27 +1000

    1. define reusable component CustomButton: with CustomButtonProps; 2. define interface CustomButtonProps;

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import { Hero } from '@/components'
 import Image from 'next/image'
-import { Hero } from './components'
+
 
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-     <Hero />
+      <Hero />
     </main>
   )
 }
---
+++ types/index.ts
@@ -0,0 +1,8 @@
+import { MouseEventHandler } from "react";
+
+/// ? is optional
+export interface CustomButtonProps {
+    title: string,
+    containerStyles?: string,
+    handleClick?: MouseEventHandler<HTMLButtonElement>
+}
